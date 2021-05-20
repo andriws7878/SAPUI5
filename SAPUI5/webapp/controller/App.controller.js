ae97556daf2e7a8cf9bd1146df61b151363aec8d
andriws7878 <andriws7878@gmail.com>
2021-05-20 01:31:39 +0000

    06_aspectos_aplicacion

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,29 +1,21 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.m.MessageToast} MessageToast
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
      */
-    function (Controller, MessageToast, Models, ResourceModel) {
+    function (Controller, MessageToast) {
         "use strict";
         return Controller.extend("andriws.SAPUI5.controller.App", {
 
             onInit: function () {
-                
+
             },
 
-            onShowHello: function () {
-                //read text from model i18n
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                //read property from dta model
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
+            onOpenDialogHeader: function () {
+                this.getOwnerComponent().openHelloDialog();
             }
         });
     });
